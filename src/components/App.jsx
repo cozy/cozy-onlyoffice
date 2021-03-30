9e358b480652268e1e0e0d8ba740ba66ee757ea2
JF-Cozy <julien.ficheux@cozycloud.cc>
2021-03-30 12:15:35 +0200

    feat: Remove example components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,14 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
-import { Route, Switch, Redirect, HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Layout, Main, Content } from 'cozy-ui/react/Layout'
 import { Sprite as IconSprite } from 'cozy-ui/react/Icon'
-
-import Sidebar from './Sidebar'
-import Todos from './Todos'
-import Hello1 from './HelloViews/Hello1'
-import Hello2 from './HelloViews/Hello2'
 
 const App = () => (
   <HashRouter>
     <Layout>
-      <Sidebar />
       <Main>
-        <Content className="app-content">
-          <Switch>
-            <Route path="/todos" component={Todos} />
-            <Route path="/viewhello1" component={Hello1} />
-            <Route path="/viewhello2" component={Hello2} />
-            <Redirect from="/" to="/todos" />
-            <Redirect from="*" to="/todos" />
-          </Switch>
-        </Content>
+        <Content className="app-content" />
       </Main>
       <IconSprite />
     </Layout>
